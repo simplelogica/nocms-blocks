250bcddf05cf8a21a1ffab56b96e52095b0693d6
Luis Mendo <luis.mendo@the-cocktail.com>
2014-12-22 10:31:08 +0100

    Adding admin views.

---
+++ app/decorators/models/no_cms/blocks/block_decorator.rb
@@ -0,0 +1,3 @@
+NoCms::Blocks::Block.class_eval do
+  attr_accessor :no_cms_admin_template
+end
--- lib/no_cms/blocks/engine.rb
+++ lib/no_cms/blocks/engine.rb
@@ -5,6 +5,13 @@ module NoCms
   module Blocks
     class Engine < ::Rails::Engine
       isolate_namespace NoCms::Blocks
+
+      config.to_prepare do
+        Dir.glob(NoCms::Blocks::Engine.root + "app/decorators/**/*_decorator*.rb").each do |c|
+          require_dependency(c)
+        end
+      end
+
     end
   end
 end
