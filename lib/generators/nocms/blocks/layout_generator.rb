292ad711ca395efc48518193361821bca86d5094
David J. Brenes <davidjbrenes@gmail.com>
2015-06-30 18:05:31 +0200

    Prepending _ to sass and html partials

--- lib/generators/nocms/blocks/layout_generator.rb
+++ lib/generators/nocms/blocks/layout_generator.rb
@@ -7,12 +7,12 @@ module NoCms
       argument :name, required: true, banner: '<layout name>', desc: 'Name for the block layout'
 
       def generate_stylesheets
-        template "app/assets/stylesheets/no_cms/blocks/layout.scss.erb", File.join(destination_root, "app/assets/stylesheets/no_cms/blocks", "#{name}.scss")
+        template "app/assets/stylesheets/no_cms/blocks/layout.scss.erb", File.join(destination_root, "app/assets/stylesheets/no_cms/blocks", "_#{name}.scss")
       end
 
       def generate_views
-        template "app/views/no_cms/blocks/blocks/layout.html.erb", File.join(destination_root, "app/views/no_cms/blocks/blocks", "#{name}.html.erb")
-        template "app/views/no_cms/admin/blocks/blocks/layout.html.erb", File.join(destination_root, "app/views/no_cms/admin/blocks/blocks", "#{name}.html.erb")
+        template "app/views/no_cms/blocks/blocks/layout.html.erb", File.join(destination_root, "app/views/no_cms/blocks/blocks", "_#{name}.html.erb")
+        template "app/views/no_cms/admin/blocks/blocks/layout.html.erb", File.join(destination_root, "app/views/no_cms/admin/blocks/blocks", "_#{name}.html.erb")
       end
 
       def self.namespace
