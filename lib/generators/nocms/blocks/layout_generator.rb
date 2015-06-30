c331758569bab252d48799c3176988e9e4e8ed1f
David J. Brenes <davidjbrenes@gmail.com>
2015-06-30 17:59:07 +0200

    Generating an stylesheet for the layout

--- lib/generators/nocms/blocks/layout_generator.rb
+++ lib/generators/nocms/blocks/layout_generator.rb
@@ -6,6 +6,11 @@ module NoCms
 
       argument :name, required: true, banner: '<layout name>', desc: 'Name for the block layout'
 
+      def generate_stylesheets
+        template "app/assets/stylesheets/no_cms/blocks/layout.scss.erb", File.join(destination_root, "app/assets/stylesheets/no_cms/blocks", "#{name}.scss")
+      end
+
+
       def self.namespace
         "nocms:blocks:layout"
       end
