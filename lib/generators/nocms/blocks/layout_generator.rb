fc28b9c349583583a879772acbaa54623587a85f
David J. Brenes <davidjbrenes@gmail.com>
2015-07-03 11:55:10 +0200

    Layout generator had wrong name

--- lib/generators/nocms/blocks/layout_generator.rb
+++ lib/generators/nocms/blocks/layout_generator.rb
@@ -1,6 +1,6 @@
 module NoCms
   module Blocks
-    class BlockGenerator < Rails::Generators::Base
+    class LayoutGenerator < Rails::Generators::Base
 
       source_root File.expand_path("../templates/", __FILE__)
 
