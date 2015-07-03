962832e1fee7ba6d7132d1d124183673303dbbb7
David J. Brenes <davidjbrenes@gmail.com>
2015-07-03 14:36:20 +0200

    First duplication spec
    We don't test yet any internationalized scenario

--- spec/models/no_cms/blocks/block_spec.rb
+++ spec/models/no_cms/blocks/block_spec.rb
@@ -197,4 +197,39 @@ describe NoCms::Blocks::Block do
       expect(NoCms::Blocks::Block.no_drafts).to match_array no_draft_blocks
     end
   end
+
+  context "when duplicating the block" do
+    let(:block_title) { Faker::Lorem.sentence }
+    let(:block) { NoCms::Blocks::Block.create layout: 'title-long_text', title: block_title }
+
+    let(:dupped_block) { block.dup }
+
+
+    before(:all) do
+      NoCms::Blocks.configure do |config|
+        config.block_layouts = {
+          'title-long_text' => {
+            template: 'title-long_text',
+            fields: {
+              title: :string,
+              body: :text
+            }
+          }
+        }
+      end
+    end
+
+    before { dupped_block.save! }
+
+    subject { NoCms::Blocks::Block.find dupped_block.id }
+
+    it "should have the same layout" do
+      expect(subject.layout).to eq block.layout
+    end
+
+    it "should have the same text" do
+      expect(subject.title).to eq block_title
+    end
+  end
+
 end
