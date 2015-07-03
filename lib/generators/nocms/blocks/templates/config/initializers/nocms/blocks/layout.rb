3ce9397409dfde06a00c5d714e8e370e055c736c
David J. Brenes <davidjbrenes@gmail.com>
2015-07-03 11:55:58 +0200

    Dummy attribute on generated layout

--- lib/generators/nocms/blocks/templates/config/initializers/nocms/blocks/layout.rb
+++ lib/generators/nocms/blocks/templates/config/initializers/nocms/blocks/layout.rb
@@ -5,14 +5,14 @@ NoCms::Blocks.configure do |config|
                                     # used as a partial both in the front
                                     # and the admin (if you use the nocms-admin gem)
     fields: { # This is the list of fields a block with this layout would have
-  #   title: :string,
+      text: :string,
   #   long_text: :text,
   #   image: Image, # You may use another ActiveRecord classes of your own
-    }
+    },
     allow_nested_blocks: true, # A block with this layout may include a list of nested blocks
                                # This setting is actually used by nocms-admin gem to show
                                # nested forms
-    nest_levels: [0] # Some layout may not be nestable, or useful only in certain nesting level
+    nest_levels: [0], # Some layout may not be nestable, or useful only in certain nesting level
                      # Once again, this setting is used by nocms-admin gem to hide certain
                      # in nested blocks. When blank, it's assumed there's no restriction.
     cache_enabled: false # When setting cache_enabled you will be **overriding** the global cache_enabled
