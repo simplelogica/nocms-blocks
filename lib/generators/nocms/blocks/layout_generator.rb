64f8dc85e637df183fd2f6a42ab82e7d4f0b3c57
David J. Brenes <davidjbrenes@gmail.com>
2015-07-03 10:12:48 +0200

    Creating the initializer for blocks

--- lib/generators/nocms/blocks/layout_generator.rb
+++ lib/generators/nocms/blocks/layout_generator.rb
@@ -15,6 +15,10 @@ module NoCms
         template "app/views/no_cms/admin/blocks/blocks/layout.html.erb", File.join(destination_root, "app/views/no_cms/admin/blocks/blocks", "_#{name}.html.erb")
       end
 
+      def generate_initializer
+        template "config/initializers/nocms/blocks/layout.rb", File.join(destination_root, "config/initializers/nocms/blocks", "_#{name}.rb")
+      end
+
       def self.namespace
         "nocms:blocks:layout"
       end
