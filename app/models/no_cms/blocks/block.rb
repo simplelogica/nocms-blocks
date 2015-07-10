6b794a58f36024f106e6e7b90f9b110477d4720c
David J. Brenes <davidjbrenes@gmail.com>
2015-07-10 21:54:25 +0200

    Adding some more comments

--- app/models/no_cms/blocks/block.rb
+++ app/models/no_cms/blocks/block.rb
@@ -22,7 +22,6 @@ module NoCms::Blocks
     validates :fields_info, presence: { allow_blank: true }
 
     class Translation
-      attr_accessor :layout
 
       ##
       # In the translation we get only the translated fields
@@ -30,6 +29,14 @@ module NoCms::Blocks
         layout_config.translated_fields
       end
 
+      ##
+      # Sometimes the translation still won't have the globalized_model linked
+      # (e.g. before it's saved for the first time) and we must have a mechanism
+      # to be able to store a temporary layout
+      attr_accessor :layout
+
+      ##
+      # If we don't have a globalized model yet we return our temporary layout
       def layout
         globalized_model.nil? ? @layout : globalized_model.layout
       end
