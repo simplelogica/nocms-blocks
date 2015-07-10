d9ace9df8df51cd84d2bd935c40cd84b800fef5f
David J. Brenes <davidjbrenes@gmail.com>
2015-07-10 14:49:56 +0200

    Spec testing that untranslated field is stored right

--- spec/models/no_cms/blocks/i18n_blocks_spec.rb
+++ spec/models/no_cms/blocks/i18n_blocks_spec.rb
@@ -26,13 +26,45 @@ describe NoCms::Blocks::Block do
       end
     end
 
-    # before { block.save! }
-
     subject { NoCms::Blocks::Block.find block.id }
 
     it "should have retrieve the right text for each language" do
       I18n.with_locale(:es) { expect(subject.title).to eq block_title_es }
       I18n.with_locale(:en) { expect(subject.title).to eq block_title_en }
     end
+
+  end
+
+  context "when we have some untranslated fields" do
+    let(:block_title) { Faker::Lorem.sentence }
+    let!(:block) { NoCms::Blocks::Block.create! title: block_title,
+      layout: 'title-long_text',
+      translations_attributes: [
+        { locale: 'es', body: Faker::Lorem.paragraph },
+        { locale: 'en', body: Faker::Lorem.paragraph }
+      ]
+    }
+
+    before(:all) do
+      NoCms::Blocks.configure do |config|
+        config.block_layouts = {
+          'title-long_text' => {
+            template: 'title-long_text',
+            fields: {
+              title: { type: :string, i18n: false },
+              body: :text
+            }
+          }
+        }
+      end
+    end
+
+    subject { NoCms::Blocks::Block.find block.id }
+
+    it "should retrieve the right text for each language" do
+      I18n.with_locale(:es) { expect(subject.title).to eq block_title }
+      I18n.with_locale(:en) { expect(subject.title).to eq block_title }
+    end
+
   end
 end
