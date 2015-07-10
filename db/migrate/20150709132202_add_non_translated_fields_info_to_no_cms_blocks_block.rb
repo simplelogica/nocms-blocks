581213ac893e2b4e7b2b46dc9ef82a571306ce03
David J. Brenes <davidjbrenes@gmail.com>
2015-07-10 14:46:16 +0200

    Renaming the fields_info attribute for the block
    I know I shouldn't modify a migration but since it's not even pushed
    it seemed fine.
    
    We are trying to use the same names and do it in a transparent way.

--- db/migrate/20150709132202_add_non_translated_fields_info_to_no_cms_blocks_block.rb
+++ db/migrate/20150709132202_add_non_translated_fields_info_to_no_cms_blocks_block.rb
@@ -1,5 +1,5 @@
 class AddNonTranslatedFieldsInfoToNoCmsBlocksBlock < ActiveRecord::Migration
   def change
-    add_column :no_cms_blocks_blocks, :non_translated_fields_info, :longtext
+    add_column :no_cms_blocks_blocks, :fields_info, :longtext
   end
 end
