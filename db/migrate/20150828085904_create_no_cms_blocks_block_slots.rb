93de2b7066a42ec51589749ef0b862918b2b6344
David J. Brenes <davidjbrenes@gmail.com>
2015-08-28 14:46:43 +0200

    Fixing foreign keys for mysql

--- db/migrate/20150828085904_create_no_cms_blocks_block_slots.rb
+++ db/migrate/20150828085904_create_no_cms_blocks_block_slots.rb
@@ -2,10 +2,13 @@ class CreateNoCmsBlocksBlockSlots < ActiveRecord::Migration
   def change
     create_table :no_cms_blocks_block_slots do |t|
       t.belongs_to :container, polymorphic: true, index: {name: 'index_no_cms_blocks_block_slots_on_container_type_and_id'}
-      t.belongs_to :block, index: true, foreign_key: true
+      t.belongs_to :block, index: true
       t.integer :position, default: 0
 
       t.timestamps null: false
     end
+
+    add_foreign_key :no_cms_blocks_blocks, :no_cms_blocks_blocks, column: 'parent_id'
+
   end
 end
