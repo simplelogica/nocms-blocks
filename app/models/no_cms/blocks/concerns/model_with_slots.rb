d8fbe00280d70a1d91701b647c71ee706e94e00e
David J. Brenes <davidjbrenes@gmail.com>
2015-08-31 13:32:40 +0200

    Accepting nested attributes for the model with slots

--- app/models/no_cms/blocks/concerns/model_with_slots.rb
+++ app/models/no_cms/blocks/concerns/model_with_slots.rb
@@ -5,6 +5,8 @@ module NoCms::Blocks::Concerns
     self.included do
       has_many :block_slots, as: :container, class_name: 'NoCms::Blocks::BlockSlot'
       has_many :blocks, through: :block_slots
+
+      accepts_nested_attributes_for :block_slots, allow_destroy: true
     end
 
   end
