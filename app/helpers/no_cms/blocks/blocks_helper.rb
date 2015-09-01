954432260aab3372fb29bb610b41d67164d8adab
David J. Brenes <davidjbrenes@gmail.com>
2015-09-01 15:47:44 +0200

    new render_block_slot helper, useful when you work with block slots
    This helper only creates a context for the cache by providing an initial_cache_key

--- app/helpers/no_cms/blocks/blocks_helper.rb
+++ app/helpers/no_cms/blocks/blocks_helper.rb
@@ -1,6 +1,24 @@
 module NoCms
   module Blocks
     module BlocksHelper
+
+      ##
+      # This method just delegates in the render_block helper appending some
+      # info about the block_slot to the cache_key. This way, the block will
+      # have a context dependant cache in case the block depends on some info of
+      # the object container of the slot.
+      def render_block_slot block_slot, options= {}
+        return '' if block_slot.block.nil?
+
+        # if the initial_cache_key param is blank we use the blockslot cache
+        # key. Othwersise we combine the blockslot cache key with the param
+        options[:initial_cache_key] = options[:initial_cache_key].blank? ?
+          block_slot.cache_key :
+          "#{block_slot.cache_key}/#{options[:initial_cache_key]}"
+
+        render_block block_slot.block, options
+      end
+
       def render_block block, options = {}
         return if block.draft && !options[:force_render_draft]
         # If we don't have any option about cache enabled then we ask the block
