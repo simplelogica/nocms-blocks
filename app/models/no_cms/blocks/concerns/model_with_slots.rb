67720fd634df9ce49f04f0e7012787d36d7f0daf
David J. Brenes <davidjbrenes@gmail.com>
2015-09-22 12:25:32 +0200

    dependent:destroy in slot relationships so we don't leave any unused slot

--- app/models/no_cms/blocks/concerns/model_with_slots.rb
+++ app/models/no_cms/blocks/concerns/model_with_slots.rb
@@ -3,7 +3,7 @@ module NoCms::Blocks::Concerns
     extend ActiveSupport::Concern
 
     self.included do
-      has_many :block_slots, as: :container, class_name: 'NoCms::Blocks::BlockSlot'
+      has_many :block_slots, as: :container, class_name: 'NoCms::Blocks::BlockSlot', dependent: :destroy
       has_many :blocks, through: :block_slots
 
       accepts_nested_attributes_for :block_slots, allow_destroy: true
