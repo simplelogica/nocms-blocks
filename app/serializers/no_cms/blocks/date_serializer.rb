1cd67669c4432e446166c3c1ebbb508be6542c3c
David J. Brenes <davidjbrenes@gmail.com>
2015-10-05 14:39:31 +0200

    Date clas don't have any .utc method, but we don't need it!

--- app/serializers/no_cms/blocks/date_serializer.rb
+++ app/serializers/no_cms/blocks/date_serializer.rb
@@ -10,14 +10,13 @@ module NoCms::Blocks
     # class in the field configuration to parse the saved string
     def read_field
       date_string = self.container.fields_info[self.field]
-
       date_string.blank? ? nil : self.field_config[:type].parse(date_string)
     end
 
     ##
     # It stores the value in the fields_info hash
     def write_field value
-      self.container.fields_info[self.field] = value.utc.strftime('%Y-%m-%dT%H:%M:%S.%N %Z')
+      self.container.fields_info[self.field] = value.strftime('%Y-%m-%dT%H:%M:%S.%N %Z')
     end
 
   end
--- spec/models/no_cms/blocks/blocks/date_field_spec.rb
+++ spec/models/no_cms/blocks/blocks/date_field_spec.rb
@@ -13,15 +13,24 @@ describe NoCms::Blocks::Block do
               template: 'title-long_text',
               fields: {
                 starts_at: DateTime,
-                ends_at: DateTime
+                ends_at: DateTime,
+                date_field: Date,
+                time_field: Time
               }
             }
           }
         end
       end
 
-      let(:block_with_layout) { NoCms::Blocks::Block.create attributes_for(:block).merge(layout: 'title-long_text', starts_at: starts_at_example) }
-      let(:starts_at_example) {  1.day.from_now }
+      let(:block_with_layout) { NoCms::Blocks::Block.create attributes_for(:block).merge(
+        layout: 'title-long_text',
+        starts_at: starts_at_example,
+        date_field: date_example,
+        time_field: time_example)
+      }
+      let(:starts_at_example) {  1.day.from_now.to_datetime }
+      let(:date_example) {  1.day.from_now.to_date }
+      let(:time_example) {  1.day.from_now }
 
       subject { block_with_layout }
 
@@ -31,6 +40,8 @@ describe NoCms::Blocks::Block do
 
       it("should save info in layout fields") do
         expect(subject.starts_at).to eq starts_at_example
+        expect(subject.date_field).to eq date_example
+        expect(subject.time_field).to eq time_example
       end
 
       it("should save nil fields") do
@@ -39,6 +50,8 @@ describe NoCms::Blocks::Block do
 
       it("should return dates") do
         expect(subject.starts_at).to be_a DateTime
+        expect(subject.date_field).to be_a Date
+        expect(subject.time_field).to be_a Time
       end
 
 
