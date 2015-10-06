9b121cd3b4e335adfb556d3cf02a58eca89bc0a8
David J. Brenes <davidjbrenes@gmail.com>
2015-10-06 12:38:18 +0200

    Implementation for multiple active resource fields

--- app/serializers/no_cms/blocks/active_resource_serializer.rb
+++ app/serializers/no_cms/blocks/active_resource_serializer.rb
@@ -5,7 +5,8 @@ module NoCms::Blocks
   #
   # It uses the objects cache from the container in order to store the object
   # and updates the _id field.
-  class ActiveResourceSerializer < BaseSerializer
+  #
+  class ActiveResourceSerializer < BaseMultipleSerializer
 
     ##
     # If the field is not present in our objects cache we fetch it from the
@@ -14,7 +15,7 @@ module NoCms::Blocks
     # If we don't have the #{field}_id field then it builds (with build, not with
     # create) a new one and stores it in the objects cache. Later, if the block
     # is saved, this object will be saved too.
-    def read_field
+    def read_single_field
       # We get and return the object from the cached objects
       value = self.container.cached_objects[self.field.to_sym]
       return value if value
@@ -31,6 +32,30 @@ module NoCms::Blocks
     end
 
     ##
+    # If we don't have the array of objects in the cache object we fetch it from the
+    # API using the id stored in the #{field}_ids field.
+    #
+    # If we don't have the #{field}_ids field or it's blank then it returns an
+    # empty array.
+    def read_multiple_field
+
+      # We get and return the objects from the cached objects
+      values = self.container.cached_objects[self.field.to_sym]
+      return values if values
+
+      # If there was nothing in the cache then we try to get the object ids
+      field_ids = self.container.fields_info["#{field}_ids".to_sym]
+
+      # If there's any id we try to get them from the API
+      if field_ids.blank?
+        []
+      else
+        values = field_config[:type].find_all(field_ids)
+        self.container.cached_objects[field.to_sym] = values
+      end
+    end
+
+    ##
     # This method `value` to be a Hash or an ActiveResource object.
     #
     # When `value` is a Hash we load the object and assign the hash through an
@@ -39,7 +64,7 @@ module NoCms::Blocks
     #
     # When `value` is an object (or nil) we save it in the object's cache and
     # overwrite the _id field
-    def write_field value
+    def write_single_field value
       case value
       when Hash
         resource = read
@@ -54,6 +79,45 @@ module NoCms::Blocks
       end
 
       value
+    end
+
+    #
+    # This method expects `value` to be an array of Hashes or ActiveResource
+    # objects.
+    #
+    # When a hash comes with an id attribute then we get the object from the
+    # API and assign it the attributes. In other case we create a new
+    # record.
+    #
+    # When an object (or nil) is in the array we just get it.
+    #
+    # Then we get all the objects in the objects cache and save the ids in the
+    # ids field.
+    def write_multiple_field values
+      raise ArgumentError.new "Array expected for #{field} attribute" unless values.is_a? Array
+
+
+      self.container.cached_objects[field.to_sym] = values.map do |value|
+        case value
+        when Hash
+          if value.has_key? :id
+            object = field_config[:type].find(value[:id])
+            object.assign_attributes value
+            object
+          else
+            field_config[:type].new  value
+          end
+        when ActiveResource::Base, nil
+          value
+        else
+          raise ArgumentError.new "Hash, ActiveRecord or nil expected for #{field} attribute"
+        end
+      end
+
+
+
+      self.container.fields_info["#{field}_ids".to_sym] = self.container.cached_objects[field.to_sym].map(&:id)
+
     end
   end
 end
