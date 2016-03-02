e2f94194ac78fe8d2efac799aa40a296d2e8c64f
Ruben Sierra <maguilag@gmail.com>
2016-03-02 19:11:24 +0100

    Dup relations in slot blocks

--- app/models/no_cms/blocks/concerns/model_with_slots.rb
+++ app/models/no_cms/blocks/concerns/model_with_slots.rb
@@ -28,10 +28,30 @@ module NoCms::Blocks::Concerns
         options.reverse_merge!({ dup_blocks: true })
 
         duplicated = dup_without_slots
-        block_slots.each do |slot|
+
+        slot_dups = {}
+        # Order by depth to avoid between children and parents dups
+        slots = block_slots.order(:depth)
+        slots.each do |slot|
           slot.dup_block_when_duping_slot = dup_block_when_duping_slots
-          duplicated.block_slots << slot.dup
+          slot_dups[slot] = slot.dup
         end
+
+        # Resolve relations
+        slots.each do |slot|
+          slot_dup = slot_dups[slot]
+          slot_dup.container = duplicated
+          slot_parent = slot_dups[slot.parent]
+          if slot_parent
+            slot_dup.parent = slot_parent
+            slot_parent.children << slot_dup
+          end
+        end
+
+        slot_dups.each do |_, slot_dup|
+          duplicated.block_slots << slot_dup
+        end
+
         duplicated
       end
       alias_method_chain :dup, :slots
