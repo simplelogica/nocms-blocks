d9243cb646e94ef4d04a3bcae16d907396a70524
belaustegui <cristian.alvb@gmail.com>
2016-04-13 13:23:16 +0200

    Notify slots when blocks are updated
    After updating a block, it touches all its slots of the update.

--- spec/models/no_cms/blocks/blocks_slot_spec.rb
+++ spec/models/no_cms/blocks/blocks_slot_spec.rb
@@ -2,11 +2,21 @@ require 'spec_helper'
 
 describe NoCms::Blocks::BlockSlot do
 
+  let(:container) { build(:slotted_page, template: "default") }
+  let(:block) { build(:block, layout: "default") }
+  let(:slot) { build :block_slot, container: container , template_zone: 'body', block: block }
+
+  it "gets notified when block is updated" do
+    slot.save
+    sleep 1
+    block.save
+    expect(block.slots.first.updated_at).to_not eq slot.updated_at
+  end
+
   context "when the slot is built with a block with a wrong layout" do
 
     let(:container) { build(:slotted_page, template: 'default' ) }
     let(:block) { build(:block, layout: 'header1') }
-    let(:slot) { build :block_slot, container: container , template_zone: 'body', block: block }
 
     it "detects that the layout is not valid" do
       expect(slot).to_not be_valid
