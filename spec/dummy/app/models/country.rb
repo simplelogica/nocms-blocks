45e60398e25bf2f06003154e0448c2dcfa379757
David J. Brenes <davidjbrenes@gmail.com>
2016-10-06 00:35:35 +0200

    Mocking build and save calls in the ActiveResource model

--- spec/dummy/app/models/country.rb
+++ spec/dummy/app/models/country.rb
@@ -5,7 +5,17 @@ class Country < ActiveResource::Base
     self.new id: id, name: Faker::Lorem.name
   end
 
+  def self.build
+    self.new name: nil
+  end
+
   def self.find_all ids
     ids.map{|id| find id }
   end
+
+  def save
+    self.persisted = true
+    true
+  end
+
 end
