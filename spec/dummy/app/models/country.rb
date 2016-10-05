2a4c3528ce28b6b33aefdb8185d35bcc9c9855e7
David J. Brenes <davidjbrenes@gmail.com>
2016-10-05 22:59:16 +0200

    Mocking find methods in ActiveResource test model
    This way we avoid connectionsto an unexisting API

--- spec/dummy/app/models/country.rb
+++ spec/dummy/app/models/country.rb
@@ -1,3 +1,11 @@
 class Country < ActiveResource::Base
   self.site = 'http://localhost:3000'
+
+  def self.find id
+    self.new id: id, name: Faker::Lorem.name
+  end
+
+  def self.find_all ids
+    ids.map{|id| find id }
+  end
 end
