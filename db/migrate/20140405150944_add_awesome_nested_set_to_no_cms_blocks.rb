1791dccd0a0b755bbfa01030371c195d5e9bbc0e
David J. Brenes <davidjbrenes@gmail.com>
2019-05-31 22:59:18 +0200

    Found bug when running migrations in Ruby 2.5 CircleCI environment

--- db/migrate/20140405150944_add_awesome_nested_set_to_no_cms_blocks.rb
+++ db/migrate/20140405150944_add_awesome_nested_set_to_no_cms_blocks.rb
@@ -1,6 +1,6 @@
 class AddAwesomeNestedSetToNoCmsBlocks < NoCms::Blocks::Migration
   def change
-    add_column :no_cms_blocks_blocks, :parent_id, :integer
+    add_reference :no_cms_blocks_blocks, :parent, index: true
     add_column :no_cms_blocks_blocks, :lft, :integer
     add_column :no_cms_blocks_blocks, :rgt, :integer
     add_column :no_cms_blocks_blocks, :depth, :integer
